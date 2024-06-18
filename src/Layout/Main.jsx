7717245f5ece06c26bbae0c6a8e0dcece38c0cd3
Pronoy568 <pronoydas463@gamil.com>
2024-06-18 20:33:40 +0600

    Navbar is Done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "../pages/Shared/Navbar/Navbar";
 
 const Main = () => {
   return (
     <div>
-      <p>Navbar</p>
+      <NavBar></NavBar>
       <Outlet></Outlet>
       <p>Footer</p>
     </div>
---
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,61 @@
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  const navOptions = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/items">All Items</Link>
+      </li>
+      <li>
+        <Link to="/order">Order Item</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </>
+  );
+
+  return (
+    <>
+      <div className="navbar fixed z-10  bg-black text-white">
+        <div className="navbar-start text-black">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-square lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navOptions}
+            </ul>
+          </div>
+          <Link to="/" className="text-white text-xl">
+            <img src="https://i.ibb.co/F5Fqj8Q/logo7.jpg" alt="logo" />
+          </Link>
+        </div>
+        <div className="navbar-end hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navOptions}</ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
